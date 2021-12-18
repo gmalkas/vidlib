3de134d0e2a7c268023c36e8668cd465269a7514
Gabriel Malkas <gabriel.malkas@gmail.com>
2021-12-18 13:51:10 +0100

    Start Phoenix in production release

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ import Config
 # before starting your production server.
 config :vidlib, VidlibWeb.Endpoint,
   url: [host: "example.com", port: 80],
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
