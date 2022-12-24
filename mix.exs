86e348964a54a685a93702c7df5e56d69fd30bb3
Gabriel Malkas <gabriel.malkas@gmail.com>
2022-12-24 12:56:17 +0100

    Upgrade to Erlang 25 and Elixir 1.14

--- mix.exs
+++ mix.exs
@@ -38,20 +38,21 @@ defmodule Vidlib.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:quinn, "~> 1.1.3"},
+      {:briefly, github: "CargoSense/briefly"},
       {:finch, "~> 0.9.1"},
-      {:size, "~> 0.1.0"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:jason, "~> 1.2"},
       {:phoenix, "~> 1.6.2"},
-      {:timex, "~> 3.7.6"},
       {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_dashboard, "~> 0.6"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.0"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
+      {:plug_cowboy, "~> 2.5"},
+      {:quinn, "~> 1.1.3"},
+      {:size, "~> 0.1.0"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:timex, "~> 3.7.6"}
     ]
   end
 
